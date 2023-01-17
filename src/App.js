f18373c22e4713301c6072fa994b7b5136779413
Melissa <mchan120@myseneca.ca>
2023-01-17 00:17:29 -0500

    Adds option to enter input field for todo list

--- src/App.js
+++ src/App.js
@@ -9,12 +9,41 @@ function Todo({ todo }) {
   );
 };
 
+function TodoForm({ addTodo}) {
+  //start with an empty state so that the user knows they can add another to do item.
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    //exit early if the user has not entered anything
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  )
+}
+
 function App() {
   const [todos, setTodos] = React.useState([
     { text: "Create a Todo App"},
     { text: "Clean Room" },
     { text: "Workout" }
   ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text}]
+    setTodos(newTodos);
+  };
 
   return (
     <div className="app">
@@ -26,6 +55,7 @@ function App() {
             todo={todo}
             />
         ))}
+        <TodoForm addTodo={{addTodo}}/>
       </div>
     </div>
   );
